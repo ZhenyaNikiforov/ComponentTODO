3421de0159868eb2f99ba1c2781c2fdc37a989bb
Zhenya Nikiforov <12A92@mail.ru>
2022-10-31 20:46:08 +0500

    Без каталога /docs #3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import componentTodo from './components/todo/todo.js';
 new componentTodo.Todo(".TODO-container__button", ".TODO-container__text", ".TODO-container__list");
 
 import lessFiles from './styles/main.less';
