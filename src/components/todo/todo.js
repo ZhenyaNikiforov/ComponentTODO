1c42d8013da107aa86e0e280b722538dc3010a67
Zhenya Nikiforov <12A92@mail.ru>
2022-11-01 23:09:43 +0500

    Подкрашивание кнопки в зависимости от текста

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -3,11 +3,13 @@ class Todo {
         this.buttonCreateItem= this.getDomElement(buttonCreateItem); //document.querySelector(buttonCreateItem);
         this.classTextArea= classTextArea;
         this.classListOfTask= classListOfTask;
+        this.textInputField= this.getDomElement(this.classTextArea);
 
-        this.buttonCreateItem.addEventListener("click", this.handleButtonClick.bind(this, this.classTextArea, this.classListOfTask));
+        this.textInputField.addEventListener("input", this.handleFieldInput.bind(this, this.buttonCreateItem, this.textInputField));
+        this.buttonCreateItem.addEventListener("click", this.handleButtonClick.bind(this, this.classTextArea, this.classListOfTask, this.buttonCreateItem));
     }
 
-    handleButtonClick(classTextArea, classListOfTask){
+    handleButtonClick(classTextArea, classListOfTask, buttonCreateItem){
         const textElement= this.getDomElement(classTextArea);
         const textContent= textElement.value;
         if(textContent != ""){
@@ -31,12 +33,20 @@ class Todo {
             const listOfTask= this.getDomElement(classListOfTask);
             listOfTask.append(task);
             textElement.value= "";
+            buttonCreateItem.classList.remove("TODO-container__button_permitted");
         }else{
             alert("Введите символы в текстовое поле!");
         }
     }
     getDomElement(className){
         return document.querySelector(className); //-variablesDefinition.
+    }
+    handleFieldInput(buttonCreateItem, textInputField){
+        if(textInputField.value != ""){
+            buttonCreateItem.classList.add("TODO-container__button_permitted");
+        }else{
+            buttonCreateItem.classList.remove("TODO-container__button_permitted"); //console.log("pusto!");
+        }
     }
 }
 
