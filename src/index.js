06adc4f6e60c1546da3b97527dad6eb9c28f75c8
Zhenya Nikiforov <12A92@mail.ru>
2022-11-01 17:56:20 +0500

    Вот теперь по дефолту и без фигурных скобок

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import componentTodo from './components/todo/todo.js';
-new componentTodo.Todo(".TODO-container__button", ".TODO-container__text", ".TODO-container__list");
+import Todo from './components/todo/todo.js';
+new Todo(".TODO-container__button", ".TODO-container__text", ".TODO-container__list");
 
 import lessFiles from './styles/main.less';
 import htmlFiles from './pages/index.pug';
