4f7263ad727b07a018d983fbcf8f392f65946dfa
Zhenya Nikiforov <12A92@mail.ru>
2022-11-03 22:31:52 +0500

    Рефакторинг страницы

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Todo from './components/todo/todo.js';
 new Todo(".TODO-container__button", ".TODO-container__text", ".TODO-container__list", "#firstComponent");
 new Todo(".TODO-container__button", ".TODO-container__text", ".TODO-container__list", "#secondComponent");
+new Todo(".TODO-container__button", ".TODO-container__text", ".TODO-container__list", "#thirdComponent");
+new Todo(".TODO-container__button", ".TODO-container__text", ".TODO-container__list", "#fourthComponent");
 
 import lessFiles from './styles/main.less';
 import htmlFiles from './pages/index.pug';
