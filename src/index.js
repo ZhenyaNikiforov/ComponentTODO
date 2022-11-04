b6948cfaa99c6042ee900c751fc9674d322a4a0e
Zhenya Nikiforov <12A92@mail.ru>
2022-11-04 22:39:51 +0500

    Не работает инициализация на странице

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import Todo from './components/todo/todo.js';
-
-const arrayOfIdentifiers= ["#firstComponent", "#secondComponent", "#thirdComponent", "#fourthComponent"];
-arrayOfIdentifiers.forEach(currentElement => {
-    new Todo(".TODO-container__button", ".TODO-container__text", ".TODO-container__list", ".TODO-container__counter", currentElement);
-});
 
 import lessFiles from './styles/main.less';
 import htmlFiles from './pages/index.pug';
