c5f5ccb502fc228c62c32afbb7dec00a1be36ef7
Zhenya Nikiforov <12A92@mail.ru>
2022-11-12 18:57:53 +0500

    Переименование репозитория, атрибут дизейбелд, отключен алерт.

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -11,35 +11,32 @@ class Todo {
 
     handleButtonClick(domElementTextArea, domElementListOfTask, domElementButton, domElementCounterOfTask){
         const textContent= domElementTextArea.value;
-        if(textContent != ""){
-            let task= document.createElement("li");
-            task.append(textContent);
-            task.classList.add("todo-container__list-item");
-            
-            let closeButton= document.createElement("button");
-            closeButton.classList.add("todo-container__close-button");
-            closeButton.addEventListener("click", function(){
-                this.parentNode.remove();
-                const remainingTasks= domElementListOfTask.childElementCount;
-                domElementCounterOfTask.lastElementChild.textContent= remainingTasks;
-            });
-            
-            const crist= document.createElement("span");
-            crist.classList.add("todo-container__crist");
-            crist.textContent="+";
-            
-            closeButton.append(crist);
-            task.append(closeButton);
-    
-            domElementListOfTask.append(task);
-            domElementTextArea.value= "";
-            domElementButton.classList.remove("todo-container__button_permitted");
+        let task= document.createElement("li");
+        task.append(textContent);
+        task.classList.add("todo-container__list-item");
+        
+        let closeButton= document.createElement("button");
+        closeButton.classList.add("todo-container__close-button");
+        closeButton.addEventListener("click", function(){
+            this.parentNode.remove();
+            const remainingTasks= domElementListOfTask.childElementCount;
+            domElementCounterOfTask.lastElementChild.textContent= remainingTasks;
+        });
+        
+        const crist= document.createElement("span");
+        crist.classList.add("todo-container__crist");
+        crist.textContent="+";
+        
+        closeButton.append(crist);
+        task.append(closeButton);
 
-            const numberOfDescendants= domElementListOfTask.childElementCount;
-            domElementCounterOfTask.lastElementChild.textContent= numberOfDescendants;
-        }else{
-            alert("Введите символы в текстовое поле!");
-        }
+        domElementListOfTask.append(task);
+        domElementTextArea.value= "";
+        domElementButton.classList.remove("todo-container__button_permitted");
+        domElementButton.setAttribute('disabled', 'disabled');
+
+        const numberOfDescendants= domElementListOfTask.childElementCount;
+        domElementCounterOfTask.lastElementChild.textContent= numberOfDescendants;
     }
     getDomElement(componentClass, className){
         const space= " ";
@@ -48,9 +45,11 @@ class Todo {
     }
     handleFieldInput(domElementButton, domElementTextArea){
         if(domElementTextArea.value != ""){
+            domElementButton.removeAttribute('disabled');
             domElementButton.classList.add("todo-container__button_permitted");
         }else{
             domElementButton.classList.remove("todo-container__button_permitted");
+            domElementButton.setAttribute('disabled', 'disabled');
         }
     }
 }
