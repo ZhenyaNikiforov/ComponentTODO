9655ae5db0621c022189d767bac67508091f591a
Zhenya Nikiforov <12A92@mail.ru>
2022-11-26 15:08:39 +0500

    Рефакторинг класса тодо, его методов, хендлеров, пробелы.

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -2,97 +2,89 @@ import {boundMethod} from 'autobind-decorator';
 
 class Todo {
 
-    selectorTextArea= '.js-todo-container__text';
-    selectorButton= '.js-todo-container__button';
-    selectorListOfTask= '.js-todo-container__list';
-    selectorCounterOfTask= '.js-todo-container__counter';
+  selectorTextArea= '.js-todo-container__text';
+  selectorButton= '.js-todo-container__button';
+  selectorListTask= '.js-todo-container__list';
+  selectorCounterTask= '.js-todo-container__counter';
 
-    constructor (domElementWrapper){
-        this.domElementWrapper= domElementWrapper;
-        this.init();
-    }
+  constructor (domElementWrapper){
+    this.domElementWrapper= domElementWrapper;
+    this.init();
+  }
     
-    init(){
-        this.elementButton= this.setDomElement(this.selectorButton);
-        this.elementTextArea= this.setDomElement(this.selectorTextArea);
-        this.elementListOfTask= this.setDomElement(this.selectorListOfTask);
-        this.elementCounterOfTask= this.setDomElement(this.selectorCounterOfTask);
+  init(){
+    this.button= this.setDomElement(this.selectorButton);
+    this.textArea= this.setDomElement(this.selectorTextArea);
+    this.listTask= this.setDomElement(this.selectorListTask);
+    this.counterTask= this.setDomElement(this.selectorCounterTask);
 
-        this.handleButtonClick();
-        this.handleFieldInput();
+    this.bindEvent();
+  }
+
+  bindEvent(){
+    this.handleButtonClick();
+    this.handleFieldInput();
+  }
+
+  setDomElement(selectorName){
+    return this.domElementWrapper.querySelector(selectorName);
+  }
+
+  handleButtonClick(){
+    this.button.addEventListener('click', this.createTask);
+  }
+
+  @boundMethod
+  createTask(){
+    const textContent= this.textArea.value;
+    let task= document.createElement('li');
+    task.append(textContent);
+    task.classList.add('todo-container__list-item');
+    
+    let closeButton= document.createElement('button');
+    closeButton.classList.add("todo-container__close-button");
+
+    const cross= document.createElement('span');
+    cross.classList.add("todo-container__cross");
+    cross.textContent="+";
+
+    closeButton.append(cross);
+    task.append(closeButton);
+    this.listTask.append(task);
+
+    closeButton.addEventListener('click', function(){this.parentNode.remove();});
+    closeButton.addEventListener('click', this.decrementCounter);
+
+    this.textArea.value= '';
+
+    const numberOfDescendants= this.listTask.childElementCount;
+    this.counterTask.lastElementChild.textContent= numberOfDescendants;
+
+    this.button.classList.remove('todo-container__button_permitted');
+    this.button.setAttribute('disabled', 'disabled');
+  }
+
+  @boundMethod
+  decrementCounter(){
+    const remainingTasks= this.listTask.childElementCount;
+    this.counterTask.lastElementChild.textContent= remainingTasks;
+  }
+
+  handleFieldInput(){
+    this.textArea.addEventListener('input', this.unlockButton);
+  }
+
+  @boundMethod
+  unlockButton(){
+    if(this.textArea.value !=''){
+      this.button.removeAttribute('disabled');
+      this.button.classList.add('todo-container__button_permitted');
+    }else{
+      this.button.classList.remove('todo-container__button_permitted');
+      this.button.setAttribute('disabled', 'true');
     }
-
-    setDomElement(selectorName){
-        return this.domElementWrapper.querySelector(selectorName);
-    }
-
-    handleButtonClick(){
-        this.elementButton.addEventListener('click', this.creatingATask);
-    }
-
-    @boundMethod
-    creatingATask(){
-        const textContent= this.elementTextArea.value;
-        let task= document.createElement('li');
-        task.append(textContent);
-        task.classList.add('todo-container__list-item');
-        
-        let closeButton= document.createElement('button');
-        closeButton.classList.add("todo-container__close-button");
-
-        const cross= document.createElement('span');
-        cross.classList.add("todo-container__cross");
-        cross.textContent="+";
-
-        closeButton.append(cross);
-        task.append(closeButton);
-        this.elementListOfTask.append(task);
-
-        closeButton.addEventListener('click', this.removeTask);
-        closeButton.addEventListener('click', this.decrementCounter);
-
-        this.elementTextArea.value= '';
-
-        const numberOfDescendants= this.elementListOfTask.childElementCount;
-        this.elementCounterOfTask.lastElementChild.textContent= numberOfDescendants;
-
-        this.elementButton.classList.remove('todo-container__button_permitted');
-        this.elementButton.setAttribute('disabled', 'disabled');
-    }
-
-    removeTask(){
-        this.parentNode.remove();
-    }
-
-    @boundMethod
-    decrementCounter(){
-        const remainingTasks= this.elementListOfTask.childElementCount;
-        this.elementCounterOfTask.lastElementChild.textContent= remainingTasks;
-    }
-
-    handleFieldInput(){
-        this.elementTextArea.addEventListener('input', this.buttonControl)
-    }
-
-    @boundMethod
-    buttonControl(){
-        if(this.elementTextArea.value !=''){
-            this.elementButton.removeAttribute('disabled');
-            this.elementButton.classList.add('todo-container__button_permitted');
-        }else{
-            this.elementButton.classList.remove('todo-container__button_permitted');
-            this.elementButton.setAttribute('disabled', 'disabled');
-        }
-    }
-    /*handleFieldInput(domElementWrapper){
-        if(textArea.value != ""){
-            button.removeAttribute('disabled');
-            button.classList.add("todo-container__button_permitted");
-        }else{
-            button.classList.remove("todo-container__button_permitted");
-            button.setAttribute('disabled', 'disabled');
-        }
-    }*/   
+  }
+   
 }
 
 export default Todo;
--- src/pages/home/render.js
+++ src/pages/home/render.js
@@ -1,7 +1,12 @@
 import Todo from '../../components/todo/todo.js';
 
-const nodeListOfWrapperComponents= document.querySelectorAll('.home-page__component-wrapper');
-const arrayOfWrapperComponents= Array.from(nodeListOfWrapperComponents);
-const arrayOfClassInstances= arrayOfWrapperComponents.map(function(a){return new Todo(a);});
+function renderingComponents(){
+  const nodeListOfWrapperComponents= document.querySelectorAll('.js-home-page__component-wrapper');
+  const arrayOfWrapperComponents= Array.from(nodeListOfWrapperComponents);
+  const arrayOfClassInstances= arrayOfWrapperComponents.map(function(a){return new Todo(a);});
+  return arrayOfClassInstances;
+}
+
+const arrayOfClassInstances= renderingComponents();
 
 export default arrayOfClassInstances;
