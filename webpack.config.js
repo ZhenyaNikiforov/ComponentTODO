6e3acaf50735955ecfb25ea994898415fc95a0fc
Zhenya Nikiforov <12A92@mail.ru>
2022-12-11 21:55:28 +0500

    Вот как вышло 11.12.2022

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,75 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-let mode ='development';
-if(process.env.NODE_ENV === 'production'){
-    mode='production';
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
 }
 
 module.exports = {
-    entry: './src/index.js',
-    mode: mode,
-    devServer: {
-    static: './docs'
+  entry: './src/index.js',
+  mode,
+  devServer: {
+    static: './docs',
   },
-    
-    output: {
-        path: path.resolve(__dirname, 'docs'),
-        filename: 'index.js'
+
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'index.js',
   },
-    
+
   plugins: [
-       new MiniCssExtractPlugin(),
-       new HtmlWebpackPlugin ({filename: "index.html", template: "./src/pages/home/index.pug", inject: 'body'}),
-       new HtmlWebpackPugPlugin({adjustIndent: true})
-   ],
-    
-    module: {
-        rules: [
-            {
-                test: /\.(ttf|eot|woff|woff2)$/i,
-                type: 'asset/resource',
-                generator: {filename: 'fonts/[name][ext]'}
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                type: 'asset/resource',
-                generator: {filename: 'images/[name][ext]'}
-            },
-            {
-                test: /\.less$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
-            },
-            {
-              test: /\.pug$/i,
-				use: [{loader: 'html-loader'}, {loader: 'pug-html-loader', options: {exports: false}}]
-              
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    },
-    optimization: {
-        minimizer: [new CssMinimizerPlugin(), new TerserPlugin()]
-    }
-}
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/pages/home/index.pug',
+      inject: 'body',
+    }),
+    new HtmlWebpackPugPlugin({ adjustIndent: true }),
+    new ESLintPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(ttf|eot|woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'fonts/[name][ext]' },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'images/[name][ext]' },
+      },
+      {
+        test: /\.less$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.pug$/i,
+        use: [
+          { loader: 'html-loader' },
+          { loader: 'pug-html-loader', options: { exports: false } },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+};
